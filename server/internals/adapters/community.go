885dede6b5e02a99ac7e09ef9214c82d52f67aae
okaabe <orlandi2006@hotmail.com>
2021-07-08 14:56:32 -0300

    Added community adapter

---
+++ server/internals/adapters/community.go
@@ -0,0 +1,42 @@
+package adapters
+
+import (
+	"choco/server/internals/models"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type CommunityAdapter interface {
+	Add(community *models.Community) error
+	Get(id string) (*models.Community, error)
+	Name(name string) (*models.Community, error)
+}
+
+type CommunityAdapterImpl struct {
+	Adapter *gorm.DB
+}
+
+func (this *CommunityAdapterImpl) Add(community *models.Community) error {
+	return this.Adapter.Create(community).Error
+}
+
+func (this *CommunityAdapterImpl) get(property string, value string) (*models.Community, error) {
+	var community models.Community
+
+	err := this.Adapter.First(&community, fmt.Sprintf("%s = ?", property), value).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (this *CommunityAdapterImpl) Get(id string) (*models.Community, error) {
+	return this.get("id", id)
+}
+
+func (this *CommunityAdapterImpl) Name(name string) (*models.Community, error) {
+	return this.get("name", name)
+}
