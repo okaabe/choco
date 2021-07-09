86538b70322b0f5c98fcbc22a227e977be2403a9
okaabe <orlandi2006@hotmail.com>
2021-07-09 13:36:08 -0300

    Added auth tests

--- server/tests/auth.go
+++ server/tests/auth.go
@@ -6,23 +6,64 @@ import (
 	"testing"
 )
 
-func testAuthRegister(t *testing.T, auth *auth.Auth, email string, password []byte) (*models.User, string) {
-	return nil, ""
+func testAuthRegister(t *testing.T, auth *auth.Auth, username, email string, password []byte) (*models.User, string) {
+	user, token, err := auth.Register(username, email, password)
+
+	if err != nil {
+		t.Errorf("Not expected an error to register an user: %s", err)
+	}
+
+	return user, token
 }
 
 func testAuthRewoke(t *testing.T, auth *auth.Auth, token string) *models.User {
-	return nil
+	user, err := auth.Rewoke(token)
+
+	if err != nil {
+		t.Errorf("Not expected an error to rewoke a token: %s", err)
+	}
+
+	return user
 }
 
 func testAuthInvalidRewoke(t *testing.T, auth *auth.Auth, token string) {
+	_, err := auth.Rewoke(token)
+
+	if err == nil {
+		t.Errorf("Expected an error to rewoke an invalid token: %s", err)
+	}
 }
 
 func testAuthValidAuthenticate(t *testing.T, auth *auth.Auth, email string, password []byte) string {
-	return ""
+	_, token, err := auth.Authenticate(email, password)
+
+	if err != nil {
+		t.Errorf("Not expected an error to authenticate with a valid email and password: %s", err)
+	}
+
+	return token
 }
 
 func testAuthInvalidAuthenticate(t *testing.T, auth *auth.Auth, email string, password []byte) {
+	_, _, err := auth.Authenticate(email, password)
+
+	if err == nil {
+		t.Errorf("Expected an error to authenticate with an incorrect email and password: %s", err)
+	}
 }
 
 func testAuth(t *testing.T, auth *auth.Auth) {
+	var (
+		username = "choco"
+		email    = "choco@choco"
+		password = []byte("choco")
+	)
+
+	_, token := testAuthRegister(t, auth, username, email, password)
+
+	testAuthRewoke(t, auth, token)
+	testAuthInvalidRewoke(t, auth, "a.a.a.a.a.aa.a")
+
+	testAuthValidAuthenticate(t, auth, email, password)
+	testAuthInvalidAuthenticate(t, auth, "aa..a.a.a@a.a.a.a", []byte("aoaoaoa"))
 }
