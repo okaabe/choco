202ce4bdafbf763ca94c201f37ced6f819f8b7e9
okaabe <orlandi2006@hotmail.com>
2021-07-09 22:23:53 -0300

    Added main function of the application

---
+++ server/cmd/main.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "fmt"
+
+func RunApp() {
+	fmt.Print("Hello World")
+}
--- server/main.go
+++ server/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"choco/server/cmd"
+)
+
 func main() {
-	println("Hello World")
+	cmd.RunApp()
 }
