b1e906bd0c219188c5556ed8522e1cd702feff1e
okaabe <orlandi2006@hotmail.com>
2021-07-10 21:22:50 -0300

    Added auth middlware

--- server/internals/http/middlwares/auth.go
+++ server/internals/http/middlwares/auth.go
@@ -2,6 +2,7 @@ package middlwares
 
 import (
 	"choco/server/internals/auth"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,5 +12,23 @@ type AuthMiddlware struct {
 }
 
 func (this *AuthMiddlware) Middlware(c *gin.Context) {
+	token := c.Request.Header.Get("Authorization")
+
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"err": "Bad Request",
+		})
+		return
+	}
+
+	_, err := this.Auth.Rewoke(token)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"err": "Invalid token",
+		})
+		return
+	}
+
 	c.Next()
 }
