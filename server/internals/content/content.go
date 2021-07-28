8d792de44c54a7c329e9bb4cdecf0147b21075de
okaabe <orlandi2006@hotmail.com>
2021-07-28 00:58:48 -0300

    Created the CreateCommunity method

--- server/internals/content/content.go
+++ server/internals/content/content.go
@@ -4,6 +4,7 @@ import (
 	"choco/server/internals/adapters"
 	"choco/server/internals/auth"
 	"choco/server/internals/models"
+	"errors"
 )
 
 type Content struct {
@@ -14,6 +15,35 @@ type Content struct {
 }
 
 func (this *Content) CreateCommunity(name, description, token string, nsfw, private bool) (*models.Community, error) {
+	user, rewokeErr := this.Auth.Rewoke(token)
 
-	return nil, nil
+	if rewokeErr != nil {
+		return nil, rewokeErr
+	}
+
+	community, communityErr := models.NewCommunity(name, description, user.ID, private, nsfw)
+
+	if communityErr != nil {
+		return nil, errors.New("Couldn't create the community")
+	}
+
+	member, memberErr := models.NewMember(user.ID, community.ID)
+
+	if memberErr != nil {
+		return nil, errors.New("Couldn't create the community")
+	}
+
+	communityAdapterErr := this.CommunityAdapter.Add(community)
+
+	if communityAdapterErr != nil {
+		return nil, errors.New("Couldn't save the community")
+	}
+
+	memberAdapterErr := this.MemberAdapter.Add(member)
+
+	if memberAdapterErr != nil {
+		return nil, errors.New("COuldn't create the community")
+	}
+
+	return community, nil
 }
