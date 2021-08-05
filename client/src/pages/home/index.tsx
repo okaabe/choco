fde84abc69c90d1cdc60854ba957b7eb83d713f5
okaabe <orlandi2006@hotmail.com>
2021-08-05 11:46:03 -0300

    Created home page

---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View } from '../../components/view';
+
+export const Home: React.FC = () => {
+    return (
+        <View>
+            Hello Okaabe, you are on the home page. be careful
+        </View>
+    )
+}
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -3,16 +3,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from './route';
 
-import { toast } from 'react-toastify';
+import { Home } from '../pages/home';
 
 export const ApplicationRoutes: React.FC = () => {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={() => {
-                return (
-                    <button onClick={() => toast("hello world")}>Hello World</button>
-                )
-            }}/>
+            <Route path="/" exact component={ Home }/>
         </BrowserRouter>
     )
 }
